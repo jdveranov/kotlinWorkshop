c7f5ceaecf6903094fdb8136049bea3ce52ed5e5
juan_verano <jdveranov@gmail.com>
2022-05-11 09:59:06 -0500

    Ejercicio For y uso de listas

--- src/main/kotlin/ForAndLists.kt
+++ src/main/kotlin/ForAndLists.kt
@@ -19,8 +19,17 @@ fun main() {
     months.remove("November")
     println(months)
 
+    //Suma de arrays
+    val numbers = intArrayOf(1,2,3)
+    val numbers3 = intArrayOf(4,5,6)
+    val foo2 = numbers + numbers3
+    println(foo2[5])
 
-
+    var bubbles = 0
+    while (bubbles < 50) {
+        bubbles++
+    }
+    println("$bubbles bubbles in the water\n")
 
 
 
