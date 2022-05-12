9ad938acc583494ce96d613ad53b6575928409ff
Juan David Verano <jdveranov@gmail.com>
2022-05-11 21:03:21 -0500

    Herencia y subclases

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,9 @@
 package example.myapp
 
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40 ) {
+import java.lang.Math.PI
+
+
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40 ) {
 
     init {
         println("aquarium initializing")
@@ -16,19 +19,38 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         height = (tank / (length * width)).toInt()
     }
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 l
         set(value) {
             height = (value * 1000) / (width * length)
         }
 
+    open val shape = "rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
+
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
         // 1 l = 1000 cm^3
-        println("Volume: $volume l")
+        println("Volume: $volume l Water: $water l (${water/volume*100.0}% full)")
     }
 
 
 }
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+        override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+        override var water = volume * 0.8
+        override val shape = "cylinder"
+}
+
--- src/main/kotlin/example/myapp/main.kt
+++ src/main/kotlin/example/myapp/main.kt
@@ -1,10 +1,11 @@
 package example.myapp
 
+//
 fun buildAquarium() {
-    val aquarium6 = Aquarium(numberOfFish = 29)
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 
 }
 
