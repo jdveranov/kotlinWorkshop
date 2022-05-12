706b0518bcb841ebb5075ac8d36a5965909e3246
Juan David Verano <jdveranov@gmail.com>
2022-05-11 20:28:51 -0500

    Creación de constructores

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,13 +1,34 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 60
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40 ) {
+
+    init {
+        println("aquarium initializing")
+    }
+
+
+    //Secondary constructor
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+    var volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 l
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l")
     }
+
+
 }
--- src/main/kotlin/example/myapp/main.kt
+++ src/main/kotlin/example/myapp/main.kt
@@ -1,8 +1,11 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
+
 }
 
 fun main() {
