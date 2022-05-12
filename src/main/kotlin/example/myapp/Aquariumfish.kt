b44c78e768757d278f78681ae6d7cb3fbf6fd163
Juan David Verano <jdveranov@gmail.com>
2022-05-11 22:42:13 -0500

    Sealed class

--- src/main/kotlin/example/myapp/Aquariumfish.kt
+++ src/main/kotlin/example/myapp/Aquariumfish.kt
@@ -29,3 +29,14 @@ class PrintingFishAction(val food: String) : FishAction {
         println(food)
     }
 }
+
+sealed class Seal
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal): String {
+    return when(seal) {
+        is Walrus -> "walrus"
+        is SeaLion -> "sea lion"
+    }
+}
